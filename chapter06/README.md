3897bf8c751b1a42307aa78eb9112a96b39e7d05
Chanho Jung <skylogin@gmail.com>
2017-09-10 18:25:51 +0900

    add README & CRUD using react

---
+++ chapter06/README.md
@@ -0,0 +1,1 @@
+# review system using loopback.io
--- chapter06/common/models/review.js
+++ chapter06/common/models/review.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function(Review) {
-  console.log(Review);
+module.exports = function(Review){
+  Review.disableRemoteMethd("count", true);
+  Review.disableRemoteMethd("exists", true);
+  Review.disableRemoteMethd("findOne", true);
+  Review.disableRemoteMethd("createChangeStream", true);
+  Review.disableRemoteMethd("updateAll", true);
 };
